2fb67c1d4dc661b2a825d64c79f9fabf667f47cf
Pranav Praveen Mishra <pm1965@srmist.edu.in>
2026-06-07 23:27:28 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (98.90%) - LeetSync

---
+++ 792-binary-search/binary-search.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+         int n = nums.size();
+        int start =0;
+        int end = n-1;
+        int ans = -1;
+        int k = target;
+        
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(nums[mid]==k){
+                return mid;
+                end = mid -1;
+            }
+            else if(nums[mid]<k){
+                start = mid+1;
+            }
+            else if( nums[mid]>k){
+                end = mid -1;
+            }
+        }
+        return ans;
+    }
+};
