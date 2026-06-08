db2e95709f6d29ad7bf4cc24849ab2a25add396e
Pranav Praveen Mishra <pm1965@srmist.edu.in>
2026-06-09 02:08:09 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (81.86%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j=0;
-        for(int i = 0 ; i< nums.size(); i++ ){
+        int j =0;
+        int n = nums.size();
+        for(int i = 0 ; i<n ; i++){
             if(nums[i]!=0){
-                swap(nums[i], nums[j]);
-                j++;
+                swap(nums[i],nums[j]);
+                j++;}
             }
         }
-   
-    }
 };
