7326bba697a51f82f33bff7b6f8b9e1d05498d9b
Pranav Praveen Mishra <pm1965@srmist.edu.in>
2026-06-10 08:27:44 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (54.53%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
@@ -3,8 +3,8 @@ public:
     int diagonalSum(vector<vector<int>>& mat) {
         int n = mat.size();
         int sum =0;
-        for(int i = 0; i< n ; i++){
-            for(int j = 0; j<n; j++){
+        
+     /*       for(int j = 0; j<n; j++){
                 if(i==j){
                     sum+= mat[i][j];
                 }
@@ -13,7 +13,18 @@ public:
                 }
             }
         }
+        return sum;*/
+        for(int i = 0; i< n ; i++){
+            sum+= mat[i][i];
+            if(i!=n-i-1){
+                sum+=mat[i][n-i-1];
+            }
+        }
         return sum;
+        
+
+
+
         
     }
 };
